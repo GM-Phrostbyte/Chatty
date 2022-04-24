161da6d11464692e9f79b6a22cb018adbaa850b7
connor-li <77080536+connor-li@users.noreply.github.com>
2022-04-24 12:00:31 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Strawberry yo yo
+          Strawberry
         </p>
         <a
           className="App-link"
