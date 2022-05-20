540fbb7feef71c61b9625f8878e4d0d0e277d3f5
Jenna Xiao <jenna_xiao@hotmail.com>
2022-05-20 06:54:34 -0400

    new chat modal done

--- src/screens/Header.js
+++ src/screens/Header.js
@@ -48,16 +48,14 @@ function Header() {
                     <BsBoxArrowRight/>
                   </button>
                 </div>
-                <button className = "newChat btn btn-primary d-flex align-items-center justify-content-center" onClick = {toggleChat}>
+                <button 
+                  className = "newChat btn btn-primary d-flex align-items-center justify-content-center" 
+                  onClick = {() => setNewChatShow(true)}>
                   <BsPlusLg/>
                 </button>
             </div>
 
-            <Button variant="primary" onClick={() => setNewChatShow(true)}>
-              Launch vertically centered modal
-            </Button>
-
-            <MyVerticallyCenteredModal
+            <ChatPanel
               show={newChatShow}
               onHide={() => setNewChatShow(false)}
             />
@@ -66,126 +64,12 @@ function Header() {
               show={logOutShow}
               onHide={() => setLogOutShow(false)}
             />
-            <ChatPanel visible={chatVisibility} toggle={toggleChat}></ChatPanel>
         </div>
     );
 }
 
+
 function ChatPanel(props) {
-    const [newFriendEmail, setNewFriendEmail] = useState('');
-    const [myName, setMyName] = useState('');
-    const [newFriendName, setNewFriendName] = useState('');
-    const [errors, setErrors] = useState('');
-
-    const togglePanel = () => {
-        resetForm();
-        props.toggle();
-    }
-
-    const resetForm = () => {
-        setNewFriendEmail('');
-        setMyName('');
-        setNewFriendName('');
-        setErrors('');
-    }
-
-
-    const validateForm = async() => {
-
-        const myEmail = auth.currentUser.email;
-        const snapshot = await usersRef.doc(newFriendEmail).get();
-
-        const ref = usersRef.doc(myEmail).collection('chats');
-
-        const snapshot2 = await ref.where('email', '==', newFriendEmail).get();
-
-        if (!snapshot.exists) {
-            setErrors("This user does not exist!");
-        } else if (!snapshot2.empty) {
-            setErrors("You've already messaged this user!");
-        } else {
-            return false;
-        }
-        return true;
-    }
-
-    const handleInputChange = (e) => {
-        setNewFriendEmail(e.target.value);
-    }
-
-
-    const addData = async () => {
-        const myEmail = auth.currentUser.email;
-        const chatID = firestore.collection('chats').doc();
-        const snapshot = await usersRef.doc(myEmail).get(); 
-        setMyName(snapshot.data().name);
-        const snapshot2 = await usersRef.doc(newFriendEmail).get();
-        setNewFriendName(snapshot2.data().name);
-
-        console.log(errors);
-
-        await chatID.collection('users').doc('emails').set({
-            userEmail1 : myEmail,
-            userEmail2 : newFriendEmail
-        });
-
-        await usersRef.doc(myEmail).collection('chats').doc(chatID.id).set({
-            isRead: false,
-            lastMessage: '',
-            time: '',
-            name: newFriendName,
-            email: newFriendEmail 
-        });
-
-        await usersRef.doc(newFriendEmail).collection('chats').doc(chatID.id).set({
-            isRead: false,
-            lastMessage: '',
-            time: '',
-            name: myName, 
-            email: myEmail
-        });
-    }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const errorsRet = await validateForm();
-
-
-        // basically checking if there is an error or not
-        if (!errorsRet) {
-            await addData();
-            setErrors('');
-            togglePanel(); 
-        }
-    }
-
-    return (
-        <div className = 'chatPanel container d-flex justify-content-center align-items-center flex-column' style = {{visibility: props.visible ? 'visible' : 'hidden'}}>
-            <div className = 'modalHeader container d-flex justify-content-center align-items-center'>
-                <button className="x" onClick={togglePanel}>x</button>
-                <p className = "modalHeaderText">New Message</p>
-            </div>
-            <form onSubmit={handleSubmit} className="form-actions">
-                <div className = 'newMessage'>
-                    <input 
-                        type='email' 
-                        className="newUser form-control" 
-                        placeholder="Email" 
-                        value = {newFriendEmail} 
-                        onChange = {handleInputChange}
-                    />
-                    <p className="errors">{errors}</p>
-                </div>
-                <div className = 'newMessageSubmit container'>
-                    <button type='submit' className='next btn btn-primary'>NEXT</button>
-                </div>
-            </form>
-        </div>
-    )
-}
-
-
-function MyVerticallyCenteredModal(props) {
 
   const [newFriendEmail, setNewFriendEmail] = useState('');
   const [myName, setMyName] = useState('');
@@ -286,7 +170,7 @@ function MyVerticallyCenteredModal(props) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <form onSubmit={handleSubmit} className="form-actions">
+        <form onSubmit={handleSubmit} className="form-actions d-flex justify-content-center align-items-center flex-column">
           <div className='newMessage'>
             <input
               type='email'
